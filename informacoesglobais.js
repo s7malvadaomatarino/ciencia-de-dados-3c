771562df5ab61a2dbd8e886fd9031c192d1f8cff
gabriel <107579931+s7malvadaomatarino@users.noreply.github.com>
2024-11-21 16:45:22 -0300

    21/11

--- informacoesglobais.js
+++ informacoesglobais.js
@@ -1,1 +1,9 @@
 const url = 'https://raw.githubusercontent.com/s7malvadaomatarino/ciencia-de-dados-3c/refs/heads/main/dados-globais.json';
+
+async function visualiarinformacoesGlobais(){
+const res =await fetch(url);
+
+const dados = await res.json();
+
+}
+visualiarinformacoesGlobais();
