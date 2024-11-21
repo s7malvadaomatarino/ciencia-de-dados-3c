464c2cc854dd3ac111db5bee45754a23b8e3e690
gabriel <107579931+s7malvadaomatarino@users.noreply.github.com>
2024-11-21 17:31:01 -0300

    21/11

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -2,18 +2,18 @@ const url = 'https://raw.githubusercontent.com/s7malvadaomatarino/ciencia-de-dad
 
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url);
-    
+
     const dados = await res.json();
-    
+
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
-    
+
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo}
     de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas
     em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-    
+
     const container = document.getElementById('graficos-container');
     container.appendChild(paragrafo);
-    }
-    
-    visualizarInformacoesGlobais();
+}
+
+visualizarInformacoesGlobais();
