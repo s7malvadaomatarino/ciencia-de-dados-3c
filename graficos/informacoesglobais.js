6b706ad24c415923cd965e184a4713d2580c3c89
gabriel <107579931+s7malvadaomatarino@users.noreply.github.com>
2024-11-21 17:35:39 -0300

    21/11

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -8,9 +8,9 @@ async function visualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
 
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo}
-    de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas
-    em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span>
+    de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas} estão conectadas</span>
+    em alguma rede social e passam em média  <span>${dados.tempo_medio} horas conectadas.</span>`
 
     const container = document.getElementById('graficos-container');
     container.appendChild(paragrafo);
